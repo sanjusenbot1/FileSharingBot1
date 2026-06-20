0f7592619e1fb7f6527a6cb6e8ad074f2e4b00d1
sanjusenbot1 <11hubgit1@gmail.com>
2026-06-20 23:48:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,16 +10,16 @@ API_ID = int(os.environ.get("API_ID", "20342933"))
 API_HASH = os.environ.get("API_HASH", "9233e5deebe6abfc9ba297a9678851be")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "8035487404"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8107093123"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://sample1:l7rxlfzxja1oVGjE@cluster0.blyhf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002327068809"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003612151502"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002601717681"))
 
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "300")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
 
 
 PORT = os.environ.get("PORT", "8080")
@@ -28,8 +28,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
 try:
-    ADMINS=[8035487404]
-    for x in (os.environ.get("ADMINS", "8035487404").split()):
+    ADMINS=[8107093123]
+    for x in (os.environ.get("ADMINS", "8107093123").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -67,7 +67,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need 
 
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(8035487404)
+ADMINS.append(8107093123)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
